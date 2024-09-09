179acd1480d3793b1f028b425872fbedc2d3c620
chrisjoel <joelebenka45@yahoo.com>
2024-09-09 04:16:31 +0100

    day8-Reusing-Infrastructure-with-Modules

--- Day3/Terraform/output.tf
+++
@@ -1,9 +0,0 @@
-# print the url of the webserver
-output "ec2_web_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "80"])
-}
-
-# print the url of the webserver
-output "ssh_connection_command" {
-  value     = join ("", ["ssh -i my_ec2_key.pem ec2-user@", aws_instance.ec2_instance.public_dns])
-}
